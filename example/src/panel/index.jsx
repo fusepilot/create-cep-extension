a0103a6187c7b1b84040b57ad5ce940d7b3f62c6
Michael Delaney <michael@fusepilot.com>
2017-02-27 15:29:49 -0800

    fixed example

--- example/src/panel/index.jsx
+++ example/src/panel/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { loadExtendscript } from 'create-cep-extension/bridge'
+import { loadExtendscript } from '../../../bridge' // remove
+// import { loadExtendscript } from 'create-cep-extension/bridge' // uncomment
 
 ReactDOM.render(
   <App />, document.getElementById('root')
