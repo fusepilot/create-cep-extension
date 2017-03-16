6d7ec374eb5ceb6769c2728862f38447c954433d
Kenneth Ormandy <hello@kennethormandy.com>
2017-03-16 14:57:56 -0700

    Adds support for custom app IDs and versions

--- templates/manifest.js
+++ templates/manifest.js
@@ -1,6 +1,8 @@
 module.exports = function ({
   bundleName = 'My Extension',
   bundleId = 'com.test.test.extension',
+  bundleAppIds = 'AEFT',
+  bundleAppVersions = '[13.0,15.9]',
   bundleVersion = '1.0.0',
   cepVersion = '6.0',
   width = '500',
@@ -8,6 +10,10 @@ module.exports = function ({
   cefParams = ['--allow-file-access-from-files', '--allow-file-access', '--enable-nodejs', '--mixed-context']
 }) {
   var commandLineParams = cefParams.map(cefParam => `<Parameter>${cefParam}</Parameter>`)
+  var appIds = Array.isArray(bundleAppIds) ? bundleAppIds : [bundleAppIds]
+  var appVersions = Array.isArray(bundleAppVersions) ? bundleAppVersions : [bundleAppVersions]
+  var hosts = appIds.map((host, i) => `<Host Name="${host}" Version="${appVersions[i]}" />`)
+
   return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <ExtensionManifest xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ExtensionBundleId="${bundleId}" ExtensionBundleName="${bundleName}" ExtensionBundleVersion="${bundleVersion}" Version="${cepVersion}">
   <ExtensionList>
@@ -15,7 +21,7 @@ module.exports = function ({
   </ExtensionList>
   <ExecutionEnvironment>
     <HostList>
-      <Host Name="AEFT" Version="[13.0,15.9]" />
+      ${hosts.join('\n      ')}
     </HostList>
     <LocaleList>
       <Locale Code="All"/>
